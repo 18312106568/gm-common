bf94fdf8b2139028f3e85ccb352cffa5fb20b0c5
18312106568 <315077558@qq.com>
2019-03-25 00:53:27 +0800

    公共类

---
+++ gm-common-base/src/main/java/gm/common/base/aop/LogPoint.java
@@ -0,0 +1,9 @@
+package gm.common.base.aop;
+
+import org.springframework.stereotype.Component;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect   //定义一个切面
+@Component
+public class LogPoint {
+}
