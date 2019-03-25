079b946b54d018b3e8ec47f5a6e2d38a12a7a678
MRB <bairongming@gzmpc.com>
2019-03-25 10:08:05 +0800

    添加计时日志

--- gm-common-base/src/main/java/gm/common/base/aop/LogPoint.java
+++ gm-common-base/src/main/java/gm/common/base/aop/LogPoint.java
@@ -1,9 +1,27 @@
 package gm.common.base.aop;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 import org.aspectj.lang.annotation.Aspect;
 
-@Aspect   //定义一个切面
+import java.util.Date;
+
+@Slf4j
+@Aspect
 @Component
 public class LogPoint {
+    @Pointcut(value = " @annotation(gm.common.base.annotation.CostLog)")
+    public void addCostPoint(){}
+
+    @Around("addCostPoint()")
+    public Object doLog(ProceedingJoinPoint jp) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object result = jp.proceed();
+        long end = System.currentTimeMillis();
+        log.info("======>{} return the request,cost time :{}",jp.getSignature().getName(),(end-start));
+        return result;
+    }
 }
